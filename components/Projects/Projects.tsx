3e4bd2e636a346c65288ff7c3a74c5528dc79289
f-pistone <francescopistonedev@gmail.com>
2024-03-18 14:00:07 +0100

    ADD printing of real projects

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -4,27 +4,49 @@ export default function Projects() {
   const projects = [
     {
       id: 1,
-      title: "Project 1",
+      image: "./images/google-forms-clone-logo.svg",
+      title: "Google Forms Clone",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptate, alias est porro quaerat sapiente velit eius quia doloremque cupiditate.",
-      image:
-        "https://images.unsplash.com/photo-1555774698-0b77e0d5fac6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 2,
-      title: "Project 2",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptate, alias est porro quaerat sapiente velit eius quia doloremque cupiditate.",
-      image:
-        "https://images.unsplash.com/photo-1555774698-0b77e0d5fac6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      id: 3,
-      title: "Project 3",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae voluptate, alias est porro quaerat sapiente velit eius quia doloremque cupiditate.",
-      image:
-        "https://images.unsplash.com/photo-1555774698-0b77e0d5fac6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      url: "http://localhost/google-forms-clone/",
+      year: "2024",
+      languages: [
+        {
+          name: "HTML",
+          textColor: "text-gray-100",
+          backgroundColor: "bg-orange-600",
+        },
+        {
+          name: "CSS",
+          textColor: "text-gray-100",
+          backgroundColor: "bg-blue-600",
+        },
+        {
+          name: "Tailwind CSS",
+          textColor: "text-gray-100",
+          backgroundColor: "bg-sky-500",
+        },
+        {
+          name: "JavaScript",
+          textColor: "text-gray-100",
+          backgroundColor: "bg-yellow-400",
+        },
+        {
+          name: "jQuery",
+          textColor: "text-gray-100",
+          backgroundColor: "bg-sky-700",
+        },
+        {
+          name: "PHP",
+          textColor: "text-gray-100",
+          backgroundColor: "bg-indigo-500",
+        },
+        {
+          name: "MySQL",
+          textColor: "text-gray-100",
+          backgroundColor: "bg-sky-600",
+        },
+      ],
     },
   ];
 
@@ -33,7 +55,7 @@ export default function Projects() {
       <div className="max-w-[1200px] mx-auto my-0">
         <div className="px-5 py-[150px]">
           <h2 className="text-center text-3xl mb-10">Projects</h2>
-          <ul className="grid grid-cols-1 md:grid-cols-3 gap-5">
+          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
             {projects.map((project) => (
               <li key={project.id}>
                 <ProjectCard project={project}></ProjectCard>
