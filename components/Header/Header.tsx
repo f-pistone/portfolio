7c8173885e5524e75a629e2094999a174a8a5ef4
f-pistone <francescopistonedev@gmail.com>
2024-03-23 12:20:02 +0100

    ADD container for the Header and the Footer

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,9 @@ import Navbar from "../Navbar/Navbar";
 export default function Header() {
   return (
     <header className="p-5 w-full h-[100px] bg-white border-b fixed top-0 left-0 z-[999]">
-      <Navbar></Navbar>
+      <div className="max-w-[2000px] mx-auto my-0">
+        <Navbar></Navbar>
+      </div>
     </header>
   );
 }
